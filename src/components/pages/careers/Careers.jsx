af0921d968209ce9fdb390c1d74be8cf4f42b51a
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-03-28 21:29:44 +0530

    3

--- src/components/pages/careers/Careers.jsx
+++ src/components/pages/careers/Careers.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import HeroSec from './careerComponents/HeroSec'
 import OurBenefits from './careerComponents/OurBenefits'
+import Certification from './careerComponents/Certification'
+import OurTalent from './careerComponents/OurTalent'
+import TeamGallery from './careerComponents/TeamGallery'
 
 const Careers = () => {
   return (
     <>
         <HeroSec/>
+        <Certification/>
+        <OurTalent/>
+        <TeamGallery/>
         <OurBenefits/>
     </>
   )
